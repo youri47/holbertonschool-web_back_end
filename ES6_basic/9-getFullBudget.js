9c53ed36f1ccfe16bec1994cd2dd816aee0eaa5f
youri47 <youri93400@gmail.com>
2026-04-30 10:18:22 +0000

    add task 1-12

---
+++ ES6_basic/9-getFullBudget.js
@@ -0,0 +1,14 @@
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
