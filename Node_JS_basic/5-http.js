0d109d7a2b9869288ffb0557174dfacd45f357c3
youri47 <youri93400@gmail.com>
2026-05-13 10:27:23 +0000

    task 5 correction

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,41 +1,7 @@
 const http = require('http');
-const fs = require('fs');
+const countStudents = require('./3-read_file_async');
 
 const DB_FILE = process.argv[2];
-
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      const students = lines.slice(1);
-
-      const output = [];
-      output.push(`Number of students: ${students.length}`);
-
-      const fields = {};
-      students.forEach((student) => {
-        const [firstname, , , field] = student.split(',');
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstname);
-      });
-
-      for (const field in fields) {
-        if (Object.prototype.hasOwnProperty.call(fields, field)) {
-          output.push(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-        }
-      }
-
-      resolve(output.join('\n'));
-    });
-  });
-}
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -44,11 +10,18 @@ const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
+    // On capture la sortie console.log produite par countStudents
+    const lines = ['This is the list of our students'];
+    const originalLog = console.log;
+    console.log = (msg) => lines.push(msg);
+
     countStudents(DB_FILE)
-      .then((report) => {
-        res.end(`This is the list of our students\n${report}`);
+      .then(() => {
+        console.log = originalLog;
+        res.end(lines.join('\n'));
       })
       .catch((err) => {
+        console.log = originalLog;
         res.end(`This is the list of our students\n${err.message}`);
       });
   } else {
